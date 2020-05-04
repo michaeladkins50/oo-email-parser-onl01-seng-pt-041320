bff14fff7904a2e39b6b8eb8956a152c2fa249be
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-04 18:24:38 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,8 +10,8 @@ class EmailAddressParser
     end
 
     def parse
-      email_array = @email.split(/[, ]/).uniq
-      email_array.reject! {|element| element.empty?}
-      email_array
+      email_split = @email.split(/[, ]/).uniq
+      email_split.reject! {|element| element.empty?}
+      email_split
     end
 end
